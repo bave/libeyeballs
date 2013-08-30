5d8ddc17eacdbde1fd8763b1be8a8fcfa03cecd0
bave <inoue.tomoya@gmail.com>
2013-08-30 20:13:19 +0900

    update

--- src/create.sh
+++ src/create.sh
@@ -3,59 +3,71 @@
 TERGET=$1
 OSNAME=`uname -s`
 
-if [ ${OSNAME} == 'Darwin' ]; then
+if [ ${OSNAME} = 'Darwin' ]; then
 
-    if [ ${TERGET} == 'libeyeballs' ]; then
+    if [ ${TERGET} = 'libeyeballs' ]; then
 
-    echo 'g++ -O3 -Wall -shared eyeballs.cpp -o libeyeballs.dylib'
-    echo 'g++ -g -Wall -o test test.cpp -L./ -leyeballs'
-    eval 'g++ -Wall -shared eyeballs.cpp -o libeyeballs.dylib'
-    eval 'g++ -g -Wall -o test test.cpp -L./ -leyeballs'
+        echo 'g++ -Wall -O3 -dynamiclib eyeballs.cpp -o libeyeballs.dylib'
+        echo 'g++ -Wall -o test test.cpp -L./ -leyeballs'
+        eval 'g++ -Wall -O3 -dynamiclib eyeballs.cpp -o libeyeballs.dylib'
+        eval 'g++ -Wall -o test test.cpp -L./ -leyeballs'
 
-    elif [ ${TERGET} == 'debug' ]; then
+    elif [ ${TERGET} = 'debug' ]; then
 
-    echo 'g++ -O3 -Wall -shared eyeballs.cpp -o libeyeballs.dylib -DDEBUG'
-    echo 'g++ -g -Wall -o test test.cpp -L./ -leyeballs'
-    eval 'g++ -Wall -shared eyeballs.cpp -o libeyeballs.dylib -DDEBUG'
-    eval 'g++ -g -Wall -o test test.cpp -L./ -leyeballs'
+        echo 'g++ -g -Wall -dynamiclib eyeballs.cpp -o libeyeballs.dylib -DDEBUG'
+        echo 'g++ -g -Wall -o test test.cpp -L./ -leyeballs'
+        eval 'g++ -g -Wall -dynamiclib eyeballs.cpp -o libeyeballs.dylib -DDEBUG'
+        eval 'g++ -g -Wall -o test test.cpp -L./ -leyeballs'
 
-    fi
+    elif [ ${TERGET} = 'install' ]; then
 
-elif [ ${OSNAME} == 'Linux' ]; then
+        echo 'pleas run following command'
+        echo 'cp ./libeyeballs.hpp /usr/local/include/'
+        echo 'cp ./libeyeballs.so  /usr/local/lib/'
 
-    if [ ${TERGET} == 'libeyeballs' ]; then
+    elif [ ${TERGET} = 'deinstall' ]; then
 
-    echo 'g++ -O3 -Wall -shared eyeballs.cpp -o libeyeballs.so -fPIC'
-    echo 'g++ -g -Wall -o test test.cpp -L./ -leyeballs'
-    eval 'g++ -Wall -shared eyeballs.cpp -o libeyeballs.so -fPIC'
-    eval 'g++ -g -Wall -o test test.cpp -L./ -leyeballs'
-
-    elif [ ${TERGET} == 'debug' ]; then
-
-    echo 'g++ -O3 -Wall -shared eyeballs.cpp -o libeyeballs.so -fPIC'
-    echo 'g++ -g -Wall -o test test.cpp -L./ -leyeballs'
-    eval 'g++ -Wall -shared eyeballs.cpp -o libeyeballs.so -fPIC'
-    eval 'g++ -g -Wall -o test test.cpp -L./ -leyeballs'
+        echo 'pleas run following command'
+        echo 'rm -f /usr/local/include/libeyeballs.hpp'
+        echo 'rm -f /usr/local/lib/libeyeballs.so'
 
     fi
 
-elif [ ${OSNAME} == 'FreeBSD' ]; then
+elif [ ${OSNAME} = 'Linux' ]; then
 
-    if [ ${TERGET} == 'libeyeballs' ]; then
+    if [ ${TERGET} = 'libeyeballs' ]; then
 
-    echo 'g++ -O3 -Wall -shared eyeballs.cpp -o libeyeballs.so -fPIC'
-    echo 'g++ -g -Wall -o test test.cpp -L./ -leyeballs'
-    eval 'g++ -Wall -shared eyeballs.cpp -o libeyeballs.so -fPIC'
-    eval 'g++ -g -Wall -o test test.cpp -L./ -leyeballs'
+        echo 'g++ -Wall -O3 -shared eyeballs.cpp -o libeyeballs.so -fPIC'
+        echo 'g++ -Wall -o test test.cpp -L./ -leyeballs'
+        eval 'g++ -Wall -O3 -shared eyeballs.cpp -o libeyeballs.so -fPIC'
+        eval 'g++ -Wall -o test test.cpp -L./ -leyeballs'
 
-    elif [ ${TERGET} == 'debug' ]; then
+    elif [ ${TERGET} = 'debug' ]; then
 
-    echo 'g++ -O3 -Wall -shared eyeballs.cpp -o libeyeballs.so -fPIC'
-    echo 'g++ -g -Wall -o test test.cpp -L./ -leyeballs'
-    eval 'g++ -Wall -shared eyeballs.cpp -o libeyeballs.so -fPIC'
-    eval 'g++ -g -Wall -o test test.cpp -L./ -leyeballs'
+        echo 'g++ -g -Wall -shared eyeballs.cpp -o libeyeballs.so -fPIC'
+        echo 'g++ -g -Wall -o test test.cpp -L./ -leyeballs'
+        eval 'g++ -g -Wall -shared eyeballs.cpp -o libeyeballs.so -fPIC'
+        eval 'g++ -g -Wall -o test test.cpp -L./ -leyeballs'
 
-    fi 
+    elif [ ${TERGET} = 'install' ]; then
+
+        cp ./libeyeballs.hpp /usr/local/include/
+        cp ./libeyeballs.so  /usr/local/lib/
+        nice ldconfig
+        echo 'install complete.'
+
+    elif [ ${TERGET} = 'deinstall' ]; then
+
+        rm -f /usr/local/include/libeyeballs.hpp
+        rm -f /usr/local/lib/libeyeballs.so
+        nice ldconfig
+        echo 'deinstall complete.'
+
+    fi
+
+elif [ ${OSNAME} = 'FreeBSD' ]; then
+
+    :
 
 else 
     :
